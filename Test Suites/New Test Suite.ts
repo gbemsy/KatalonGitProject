1ba9eb41f4a5bd279bdd6ab86e74b0f35b47b569
Gbemi <Gbemi@gbemisola.My4G>
2019-02-11 21:55:47 +0100

    commit2

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5a3a51f3-a8a6-473d-b5b3-f1163c19fa69</testSuiteGuid>
+</TestSuiteEntity>
